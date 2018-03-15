e82e10548c80891a0dc372ae51735e3ab38bbc48
oh eun Kwon <dhdms6512@gmail.com>
2018-03-15 20:54:33 +0900

    add

---
+++ C#/Assignment/Assignment/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mkAll = new System.Windows.Forms.Button();
+            this.mkSep = new System.Windows.Forms.Button();
+            this.mkbtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // mkAll
+            // 
+            this.mkAll.Location = new System.Drawing.Point(12, 12);
+            this.mkAll.Name = "mkAll";
+            this.mkAll.Size = new System.Drawing.Size(75, 23);
+            this.mkAll.TabIndex = 0;
+            this.mkAll.Text = "한방에";
+            this.mkAll.UseVisualStyleBackColor = true;
+            this.mkAll.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mkSep
+            // 
+            this.mkSep.Location = new System.Drawing.Point(178, 12);
+            this.mkSep.Name = "mkSep";
+            this.mkSep.Size = new System.Drawing.Size(75, 23);
+            this.mkSep.TabIndex = 1;
+            this.mkSep.Text = "하나씩";
+            this.mkSep.UseVisualStyleBackColor = true;
+            this.mkSep.Click += new System.EventHandler(this.mkSep_Click);
+            // 
+            // mkbtn
+            // 
+            this.mkbtn.Location = new System.Drawing.Point(352, 11);
+            this.mkbtn.Name = "mkbtn";
+            this.mkbtn.Size = new System.Drawing.Size(75, 23);
+            this.mkbtn.TabIndex = 2;
+            this.mkbtn.Text = "뉴버튼클릭";
+            this.mkbtn.UseVisualStyleBackColor = true;
+            this.mkbtn.Click += new System.EventHandler(this.mkbtn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mkbtn);
+            this.Controls.Add(this.mkAll);
+            this.Controls.Add(this.mkSep);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button mkAll;
+        private System.Windows.Forms.Button mkSep;
+        private System.Windows.Forms.Button mkbtn;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ C#/Assignment/Assignment/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment
+{
+    public partial class Form1 : Form
+    {
+        int tmp_location =1 ;
+        int tmp_location2 = 1;
+        int elapsed=0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 5; i++)
+            {
+                Button child = new Button();
+                Controls.Add(child);
+                child.Location = new Point(mkAll.Location.X,mkAll.Location.Y+(23+3)*i);
+                child.Text = "동적생성" + i + "번째";
+                child.Width = 100;
+            }
+            
+        }
+
+        private void mkSep_Click(object sender, EventArgs e)
+        {
+            Button child = new Button();
+            Controls.Add(child);
+            if(tmp_location == 1)
+            {
+                child.Location = new Point(mkSep.Location.X, mkSep.Location.Y + (23 + 3));
+                child.Text = "동적생성" + tmp_location +"번째";
+                child.Width = 100;
+                tmp_location++;
+            }
+            else
+            {
+                
+                child.Location = new Point(mkSep.Location.X, mkSep.Location.Y + (23 + 3) * tmp_location);
+                child.Text = "동적생성" + tmp_location + "번째";
+                child.Width = 100;
+                tmp_location++;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mkbtn_Click(object sender, EventArgs e)
+        {
+            Button child = new Button();
+            Controls.Add(child);
+            child.Width = 100;
+            if(tmp_location2 == 1)
+            {
+                child.Location = new Point(mkbtn.Location.X, mkbtn.Location.Y + (23 + 3));
+                child.Text = "동적생성" + tmp_location2 + "번째";
+                child.Click += mkbtn_Click;
+                tmp_location2++;
+            }
+            else
+            {
+                child.Location = new Point(mkbtn.Location.X, mkbtn.Location.Y + (23 + 3)*tmp_location2);
+                child.Text = "동적생성" + tmp_location2 + "번째";
+                child.Click += mkbtn_Click;
+                tmp_location2++;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            elapsed++;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+           
+            MessageBox.Show("버튼지울까요", "", MessageBoxButtons.YesNo);
+            Controls.Clear();
+            MessageBox.Show("경과된 시간은" + elapsed + "초입니다.");
+        }
+
+        private static MessageBoxButtons GetYesNo()
+        {
+           
+            return MessageBoxButtons.YesNo;
+        }
+    }
+}
