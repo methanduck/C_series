618276f73544dcce4f9ff5ff4553722094f2fafa
oh eun Kwon <dhdms6512@gmail.com>
2018-04-15 15:55:25 +0900

    up

--- C#/Assignment/Assignment_4/Form1.cs
+++ C#/Assignment/Assignment_4/Form1.cs
@@ -12,6 +12,8 @@ namespace Assignment_4
 {
     public partial class Form1 : Form
     {
+
+        int temp, input_Num;
         public Form1()
         {
             InitializeComponent();
@@ -25,13 +27,17 @@ namespace Assignment_4
         private void Btn_Lshift_Click(object sender, EventArgs e)
         {
             
-            DialogResult res = MessageBox.Show(Txtbox_input.Text + "을" + TxtBox_bitInput.Text + "만큼 비트이동합니다","안내", MessageBoxButtons.OKCancel);
-            if(res == DialogResult.OK)
+            DialogResult res = MessageBox.Show(Txtbox_input.Text + "을" + TxtBox_bitInput.Text + "만큼 왼쪽 비트이동합니다","안내", MessageBoxButtons.OKCancel);
+            if (res == DialogResult.OK)
             {
-               var tmp = Convert. .Parse(TxtBox_bitInput.Text.ToString()) << byte.Parse(Txtbox_input.Text.ToString());
-                Txtbox_binOrigin.Text = Convert.ToString(tmp, 2);
-                Txtbox_hexOrigin.Text = Convert.ToString(tmp, 16);
-                Txtbox_deciOrigin.Text = Convert.ToString(tmp, 10);
+                input_Num = int.Parse(Txtbox_input.Text.ToString());
+                temp = int.Parse(Txtbox_input.Text.ToString()) << int.Parse(TxtBox_bitInput.Text.ToString());
+                Txtbox_binOrigin.Text = Convert.ToString(input_Num, 2);
+                Txtbox_deciOrigin.Text = Convert.ToString(input_Num, 10);
+                Txtbox_hexOrigin.Text = Convert.ToString(input_Num, 16);
+                Txtbox_binRes.Text = Convert.ToString(temp, 2);
+                Txtbox_deciRes.Text = Convert.ToString(temp, 10);
+                Txtbox_hexRes.Text = Convert.ToString(temp, 16);
             }
             
         }
@@ -39,6 +45,20 @@ namespace Assignment_4
         private void Btn_Rshift_Click(object sender, EventArgs e)
         {
 
+            DialogResult res = MessageBox.Show(Txtbox_input.Text + "을" + TxtBox_bitInput.Text + "만큼 오른쪽 비트이동합니다", "안내", MessageBoxButtons.OKCancel);
+            if (res == DialogResult.OK)
+            {
+                input_Num = int.Parse(Txtbox_input.Text.ToString());
+                temp = int.Parse(Txtbox_input.Text.ToString()) >> int.Parse(TxtBox_bitInput.Text.ToString());
+                Txtbox_binOrigin.Text = Convert.ToString(input_Num, 2);
+                Txtbox_deciOrigin.Text = Convert.ToString(input_Num, 10);
+                Txtbox_hexOrigin.Text = Convert.ToString(input_Num, 16);
+                Txtbox_binRes.Text = Convert.ToString(temp, 2);
+                Txtbox_deciRes.Text = Convert.ToString(temp, 10);
+                Txtbox_hexRes.Text = Convert.ToString(temp, 16);
+            }
         }
+
+
     }
 }
