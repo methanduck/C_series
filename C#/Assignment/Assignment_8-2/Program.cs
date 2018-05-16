a670eb9283a6c005aae1796f8aae79ae02fa6eb1
oh eun Kwon <dhdms6512@gmail.com>
2018-05-16 14:23:04 +0900

    update

---
+++ C#/Assignment/Assignment_8-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_8_2
+{
+    
+    class Program 
+    {
+        
+        static void Main(string[] args)
+        {
+            Stack<int> stk = new Stack<int>();
+            stk.Push(1);
+            stk.Push(2);
+            stk.Push(3);
+
+            IEnumerator iterator = stk.GetEnumerator();
+            while(iterator.MoveNext())
+            {
+                object i = iterator.Current;
+                Console.WriteLine(i);
+            }
+            
+        }
+        
+    }
+}
---
+++ C#/Assignment/Assignment_8/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_8
+{
+    class Program
+    {
+        class Mylist : IEnumerable, IEnumerator
+        {
+            private int[] array;
+            int position = -1;
+
+            public Mylist()
+            {
+                array = new int[3];
+            }
+
+            public int this[int index]
+            {
+                get
+                {
+                    return array[index];
+                }
+                set
+                {
+                    if(index >= array.Length)
+                    {
+                        Array.Resize<int>(ref array, index + 1);
+                        Console.WriteLine("Array Resized : {0}", array.Length);
+                    }
+                    array[index] = value;
+                }
+            }
+
+            public object Current
+            {
+                get { return array[position]; }
+            }
+            public IEnumerator GetEnumerator()
+            {
+               foreach(int i in array)
+                {
+                    yield return (array[i]);
+                }
+            }
+
+            public bool MoveNext()
+            {
+               if(position == array.Length -1)
+                {
+                    Reset();
+                    return false;
+                }
+                position++;
+                return (position < array.Length);
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Mylist list = new Mylist();
+            for (int i = 0; i < 5; i++)
+            {
+                list[i] = i;
+            }
+
+            foreach(int i in list)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+        }
+    }
+}
