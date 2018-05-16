93319ed86badfc772210e9dec9fb92d4b6ca39c7
oh eun Kwon <dhdms6512@gmail.com>
2018-05-16 14:34:00 +0900

    up

--- C#/Assignment/Assignment_8-2/Program.cs
+++ C#/Assignment/Assignment_8-2/Program.cs
@@ -13,11 +13,21 @@ namespace Assignment_8_2
         
         static void Main(string[] args)
         {
+            int input;
             Stack<int> stk = new Stack<int>();
             stk.Push(1);
             stk.Push(2);
             stk.Push(3);
-
+            Console.WriteLine("1,2,3푸쉬 완료");
+            Console.WriteLine("스택 팝 >> {0}", stk.Pop());
+            Console.WriteLine("스택 팝 >> {0}", stk.Pop());
+            stk.Push(7);
+            Console.WriteLine("스택 푸쉬 7");
+            Console.WriteLine("스택에 푸쉬할 값 입력");
+            input = int.Parse(Console.ReadLine());
+            stk.Push(input);
+            Console.WriteLine("스택에 푸쉬 완료");
+            Console.WriteLine("---------------");
             IEnumerator iterator = stk.GetEnumerator();
             while(iterator.MoveNext())
             {
