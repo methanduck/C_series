06764b432eff90db39068b3e4559fe0e94680cb3
oh eun Kwon <dhdms6512@gmail.com>
2018-05-30 14:43:31 +0900

    up

---
+++ C#/Assignment/Assignment_10-2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_10_2
+{
+     delegate void MyDelegate(int a);
+        class Market
+        {
+            public event MyDelegate CustomerEvent;
+
+            public void BuySomething(int CustomerNo)
+            {
+                if (CustomerNo == 30)
+                {
+                    CustomerEvent(CustomerNo);
+                }
+            }
+        }
+    class Program
+    {
+        static public void MyEvent(int a)
+        {
+            Console.WriteLine("축하합니다! {0}번째 고객 이벤트에 당첨되셨습니다.",a);
+        }
+        static void Main(string[] args)
+        {
+            Market market = new Market();
+            market.CustomerEvent += new MyDelegate(MyEvent);
+            for (int customerno = 0; customerno <= 100; customerno+=10)
+            {
+                market.BuySomething(customerno);
+            }
+            
+        }
+    }
+    
+}
---
+++ C#/Assignment/Assignment_10-3/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_10_3
+{
+    public partial class Form1 : Form
+    {
+        delegate void MyDelegate();
+        public Form1()
+        {
+            InitializeComponent();
+            this.Click += new EventHandler(clickReceive);
+        }
+        private void clickReceive(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello world");
+        }
+        public class EventFormTest
+        {
+            static void Main()
+            {
+                Application.Run(new Form1());
+            }
+        }
+    }
+}
